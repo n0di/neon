96249faaf5bb0b0803ac84f36ec4c07b482fd6e3
Amankhojaev Nodirkhon <44620146+n0di@users.noreply.github.com>
2020-07-02 20:53:38 +1000

    Update selenium_likes_ins.py

--- selenium/selenium_likes_ins.py
+++ selenium/selenium_likes_ins.py
@@ -7,27 +7,19 @@ from selenium.webdriver.support import expected_conditions as EC
 browser = webdriver.Firefox()
 browser.implicitly_wait(5)
 
-browser.get('https://www.instagram.com/')
+browser.get('https://www.you_web_site/')
 
 sleep(3)
 
 username_input = browser.find_element_by_css_selector("input[name='username']")
 password_input = browser.find_element_by_css_selector("input[name='password']")
 
-username_input.send_keys("wh0ami_13")
-password_input.send_keys("insTneon`123")
+username_input.send_keys("you_login")
+password_input.send_keys("you_pass")
 
 login_button = browser.find_element_by_xpath("//button[@type='submit']")
 login_button.click()
 
-#login_link = browser.find_element_by_xpath("submit['Log in'")  
 sleep(15)
-
-
-#login_link.click()
-
-#login_button = browser.find_element_by_xpath("//button[@type='submit']")
-#login_button.click()
-
 
 browser.close()
