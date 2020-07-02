12244e09aed176349f3376f7b23ace7d044bb066
Amankhojaev Nodirkhon <44620146+n0di@users.noreply.github.com>
2020-07-02 21:28:59 +1000

    Update search&edite.py

--- search&edit_file/search&edite.py
+++ search&edit_file/search&edite.py
@@ -4,35 +4,33 @@ from __future__ import print_function
 import os, fnmatch
 import io
 
-tt = []
-ttt = []
+total_addresses = []
+total = []
 folder = []
-f = 'wp-config.php'
+search_file = 'you_file'
 
-for i in os.walk('/var/www/'):
+for i in os.walk('/dir'):
     folder.append(i)
 
 for address, dirs, files in folder:
     for file in files:
         if f in files:
-        	tt.append(address+'/'+file)
-for i in tt:
+        	total_addresses.append(address+'/'+file)
+for i in total_addresses:
 	if f == i[-13::]:
-		ttt.append(i)
+		total.append(i)
 
-# print(ttt[0])
-
-word = u"define('WP_MEMORY_LIMIT', "
-with io.open(f'{ttt[0]}') as file:
+word = u"tex_which_you_need_to_look_for"
+with io.open(f'{total[0]}') as file:
     for line in file:
         if word in line:
         	w = open('file.txt', 'w')
-        	w.write(f'{ttt[0]} \n{line}')
+        	w.write(f'{total[0]} \n{line}')
         	w.close
         elif word not in line:
-        	opf = open(f'{ttt[0]}', 'a')
-        	opf.write(f"define('WP_MEMORY_LIMIT', '128M');")
+        	opf = open(f'{total[0]}', 'a')
+        	opf.write(f"you_write_text")
         	opf.close
         	w = open('success.txt', 'w')
-        	w.write(f'{ttt[0]} \n')
+        	w.write(f'{total[0]} \n')
         	w.close
