4480e824cfdc0331e5df94d54371ccf6eb342b19
Amankhojaev Nodirkhon <44620146+n0di@users.noreply.github.com>
2020-07-13 11:42:30 +1000

    Update selenium_likes_ins.py

--- selenium/selenium_likes_ins.py
+++ selenium/selenium_likes_ins.py
@@ -4,22 +4,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-browser = webdriver.Firefox()
-browser.implicitly_wait(5)
+class bot:
+	
+	def __init__(self):
+		
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(5)
+		self.auth_inst() 
+		#self.auth_fb()
 
-browser.get('https://www.you_web_site/')
+	def auth_inst(self):
+		self.browser.get('https://www.instagram.com/')
+		sleep(3)
 
-sleep(3)
+		self.username_input = self.browser.find_element_by_css_selector("input[name='username']").send_keys("wh0ami_13")
+		self.password_input = self.browser.find_element_by_css_selector("input[name='password']").send_keys("insTneon`123")
 
-username_input = browser.find_element_by_css_selector("input[name='username']")
-password_input = browser.find_element_by_css_selector("input[name='password']")
+		self.login_button = self.browser.find_element_by_xpath("//button[@type='submit']")
+		self.login_button.click()
+		sleep(3)
 
-username_input.send_keys("you_login")
-password_input.send_keys("you_pass")
+    self.not_now = self.browser.find_element_by_css_selector('button.sqdOP:nth-child(1)').click()
+		self.not_now2 = self.browser.find_element_by_css_selector('button.aOOlW:nth-child(2)').click()
+		sleep(15)
 
-login_button = browser.find_element_by_xpath("//button[@type='submit']")
-login_button.click()
+	def auth_fb(self):
+		self.browser.execute_script("window.open('https://www.facebook.com','_blank');")
+		sleep(3)
+		self.browser.find_element_by_css_selector('input#email.inputtext.login_form_input_box').send_keys("email")
+		self.browser.find_element_by_css_selector("input#pass.inputtext.login_form_input_box").send_keys("pass")
+		self.browser.find_element_by_css_selector("input#u_0_4[@type='submit']").click()
 
-sleep(15)
+#browser.close()
 
-browser.close()
+def main():
+	b = bot()
+
+if __name__ == '__main__':
+	main()
