a9cce1d7f2fe0ba40e3f98a7d5e9b1a5532710a7
Amankhojaev Nodirkhon <44620146+n0di@users.noreply.github.com>
2020-07-13 11:44:21 +1000

    Add files via upload

---
+++ selenium/selenium_auth_fl.ru.py
@@ -0,0 +1,33 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+browser = webdriver.Firefox()
+browser.implicitly_wait(3)
+
+browser.get('https://www.fl.ru/login/')
+sleep(3)
+
+username_input = browser.find_element_by_css_selector("input[name='login']")
+password_input = browser.find_element_by_css_selector("input[name='passwd']")
+
+username_input.send_keys("amanxojayev@gmail.com")
+password_input.send_keys("flnodiryum12341")
+
+login_button = browser.find_element_by_xpath("//button[@type='submit']")
+login_button.click()
+sleep(10)
+
+filter_button = browser.find_element_by_css_selector("button.b-button.b-button_flat.b-button_flat_green")
+filter_button.click()
+sleep(50)
+
+while 1:
+	filter_button = browser.find_element_by_css_selector("button.b-button.b-button_flat.b-button_flat_green")
+	filter_button.click()
+	sleep(180)
+
+#browser.close()
